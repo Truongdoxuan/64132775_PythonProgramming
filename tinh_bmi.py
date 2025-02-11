28556c31bccc759b5a373bc7d13c1c81f04ace86
Xuân Trường <dxt8104@gmail.com>
2025-02-11 22:09:29 +0700

    create and complete tinh_bmi

---
+++ tinh_bmi.py
@@ -0,0 +1,8 @@
+#bài tập đầu tiên với ngôn ngữ python
+cannang = float(input("Nhap can nang cua ban (kg): "))
+chieucao = float(input("Nhap chieu cao cua ban (m): "))
+bmi = cannang / (chieucao * chieucao)
+print(f"Chi so BMI cua ban la: {bmi:.2f}")
+if (bmi > 29):
+    print("Ban beo phi")
+else : print("Ban binh thuong")
